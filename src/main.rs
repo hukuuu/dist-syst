d2554e02245ab87625c9b7533e4b4261095d9caf
Nikolay Karadzhov <nikolayk@flutterint.com>
2023-05-14 14:21:15 +0300

    refactor message structure

--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,86 @@
-use std::{io::{self, Write}};
+use std::io::{self, Write};
 
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug)]
-struct Message {
+struct Message<Payload> {
     src: String,
     dest: String,
-    body: Body
+    body: Body<Payload>,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
-struct Body {
-    r#type: String,
-    msg_id: u32,
+#[derive(Debug, Clone, Serialize, Deserialize)]
+struct Body<Payload> {
     in_reply_to: Option<u32>,
-    echo: Option<String>
+    msg_id: Option<u32>,
+    #[serde(flatten)]
+    payload: Payload,
 }
 
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(tag = "type")]
+#[serde(rename_all = "snake_case")]
+enum EchoPayload {
+    Echo { echo: String },
+    EchoOk { echo: String },
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(tag = "type")]
+#[serde(rename_all = "snake_case")]
+enum InitPayload {
+    Init(Init),
+    InitOk,
+}
+#[derive(Debug, Clone, Serialize, Deserialize)]
+struct Init {
+    node_id: String,
+    node_ids: Vec<String>,
+}
 
 fn main() -> io::Result<()> {
-    let mut id = 0;
-
-    let stdin = std::io::stdin();
+    let mut stdin = std::io::stdin().lines();
     let mut stdout = std::io::stdout().lock();
 
-    for line in stdin.lines() {
-        let line = line.unwrap();
-        let mut msg: Message = serde_json::from_str(&line)?;
+    let init_msg: Message<InitPayload> = serde_json::from_str(&stdin.next().expect("ni raboti")?)?;
+    let InitPayload::Init(init) = init_msg.body.payload else {
+        panic!("first message should be init");
+    };
 
-        let tmp = msg.src;
-        msg.src = msg.dest;
-        msg.dest = tmp;
-        msg.body.in_reply_to = Some(msg.body.msg_id);
-        msg.body.msg_id = id; 
-        id += 1;
+    let reply = Message {
+        src: init_msg.dest,
+        dest: init_msg.src,
+        body: Body {
+            msg_id: Some(0),
+            in_reply_to: init_msg.body.msg_id,
+            payload: InitPayload::InitOk,
+        },
+    };
+    serde_json::to_writer(&mut stdout, &reply).expect("eee");
+    stdout.write_all(b"\n").expect("ddd");
 
-        if msg.body.r#type == "init" {
-            msg.body.r#type = "init_ok".to_string();
-        }
+    eprintln!("{:?}", init);
 
-        if msg.body.r#type == "echo" {
-            msg.body.r#type = "echo_ok".to_string();
-    
-        }
+    for msg in stdin {
+        let msg = msg.unwrap();
+        let msg: Message<EchoPayload> = serde_json::from_str(&msg)?;
 
-        let mut resp = serde_json::to_string(&msg)?;
-        resp.push_str("\r\n");
-        stdout.write_all(resp.as_bytes())?;
+        let EchoPayload::Echo { echo } = msg.body.payload else {
+            panic!("invalid echo message");
+        };
 
+        let reply = Message {
+            src: msg.dest,
+            dest: msg.src,
+            body: Body {
+                msg_id: Some(0),
+                in_reply_to: init_msg.body.msg_id,
+                payload: EchoPayload::EchoOk { echo },
+            },
+        };
+
+        serde_json::to_writer(&mut stdout, &reply).expect("eee");
+        stdout.write_all(b"\n").expect("ddd");
     }
 
     Ok(())
